6d723088c1d2c5887e5aee4015edef6caebea37c
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-10-06 17:17:46 +0300

    initialize

---
+++ Lab1-1/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Lab1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 266);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(524, 168);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(572, 406);
+            this.Send.Margin = new System.Windows.Forms.Padding(4);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(123, 28);
+            this.Send.TabIndex = 1;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.dataSendEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 245);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(828, 365);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(825, 344);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "COM-port:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(569, 344);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Baud rate:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(826, 406);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(957, 406);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(569, 319);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "DataBits:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(569, 293);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Parity:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(569, 269);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "StopBits:";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(16, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(524, 198);
+            this.listView1.TabIndex = 23;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(556, 28);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(522, 198);
+            this.listView2.TabIndex = 24;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(828, 265);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 21);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "XOn";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(614, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "None";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(630, 319);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 17);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "8";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(641, 344);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 17);
+            this.label10.TabIndex = 29;
+            this.label10.Text = "9600";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 458);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Lab1-1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace Lab1_1
+{
+    public partial class
+
+        Form1 : Form
+    {
+        private SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            var portList = SerialPort.GetPortNames();
+            for (int i = 0; i < portList.Length; i++)
+            {
+                comboBox1.Items.Add(portList[i]);
+            }
+            label10.Text = "9600";
+            label9.Text = "8";
+            label3.Text = "None";
+            label2.Text = "1";
+
+            listView1.Scrollable = true;
+            listView1.View = View.Details;
+            ColumnHeader columnHeader = new ColumnHeader();
+            columnHeader.Width = listView1.Width;
+            columnHeader.Text = "Output: ";
+            listView1.Columns.Add(columnHeader);
+
+            listView2.Scrollable = true;
+            listView2.View = View.Details;
+            ColumnHeader header2 = new ColumnHeader();
+            header2.Width = listView2.Width;
+            header2.Text = "Debug information: ";
+            listView2.Columns.Add(header2);
+        }
+
+        private Boolean xOn = false;
+        private void sendData()
+        {
+            while (!xOn)
+            {
+                Thread.Sleep(100);
+            }
+
+            serialPort.RtsEnable = true;
+            serialPort.Write(textBox1.Text);
+            Thread.Sleep(100);
+            serialPort.RtsEnable = false;
+
+            byte[] bytesSent = System.Text.Encoding.ASCII.GetBytes(textBox1.Text);
+            this.Invoke((MethodInvoker)(delegate
+            {
+                textBox1.Text = "";
+                listView2.Items.Add("Bytes sent:" + bytesSent.Length);
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int baudRate = Int32.Parse(label10.Text);
+                int dataBits = Int32.Parse(label9.Text);
+                Parity parity = (Parity)Enum.Parse(typeof(Parity), label3.Text, true);
+                StopBits stopBits = (StopBits)Enum.Parse(typeof(StopBits), label2.Text, true);
+                serialPort = new SerialPort(comboBox1.SelectedItem.ToString(), baudRate, parity,
+                       dataBits, stopBits);
+                if (serialPort != null && serialPort.IsOpen)
+                    throw (new Exception());
+                serialPort.Encoding = Encoding.UTF8;
+                serialPort.Open();
+                button1.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                listView2.Items.Add("Error in port connection");
+            }
+            if (serialPort.IsOpen)
+            {
+                comboBox1.Enabled = false;
+
+                listView2.Items.Add("Port is opened");
+                listView2.Items.Add("Parity: " + label3.Text);
+                listView2.Items.Add("COM port: " + comboBox1.SelectedItem);
+                listView2.Items.Add("Baudrate: " + label10.Text);
+                listView2.Items.Add("StopBits: " + label2.Text);
+                listView2.Items.Add("DataBits: " + label9.Text);
+            }
+            serialPort.DataReceived += delegate
+            {
+                this.Invoke((MethodInvoker)(delegate()
+                {
+                    string message = serialPort.ReadExisting();
+                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes(message);
+                    if (bytes.Length == 1 && bytes[0] == 0x13)
+                    {
+                        xOn = false;
+                    }
+                    else if (bytes.Length == 1 && bytes[0] == 0x11)
+                    {
+                        xOn = true;
+                    }
+                    else
+                    {
+                        listView2.Items.Add("Bytes recieved:" + bytes.Length);
+                        listView1.Items.Add(message);
+                    }
+
+                }));
+
+            };
+
+        }
+
+        private void dataSendEvent(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen && serialPort.BytesToRead == 0)
+            {
+                Thread thread = new Thread(sendData);
+                thread.Start();
+            }
+            else listView2.Items.Add("To send message connect ports first!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                listView2.Items.Add("Port is closed");
+                button1.Enabled = true;
+            }
+            comboBox1.Visible = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Console.WriteLine("State: " + checkBox1.CheckState);
+                if (checkBox1.CheckState == CheckState.Checked)
+                {
+                    var xOnByte = new byte[] { 0x11 };
+                    serialPort.RtsEnable = true;
+                    serialPort.Write(xOnByte, 0, 1);
+                    Thread.Sleep(100);
+                    serialPort.RtsEnable = false;
+                }
+                if (checkBox1.CheckState == CheckState.Unchecked)
+                {
+                    var xOffByte = new byte[] { 0x13 };
+                    serialPort.RtsEnable = true;
+                    serialPort.Write(xOffByte, 0, 1);
+                    Thread.Sleep(100);
+                    serialPort.RtsEnable = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                listView2.Items.Add("You aren't connected to any port");
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+
+
